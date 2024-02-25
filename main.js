7d3a1341166155c976a022b69c869664c0f61c91
DiogoEngh <diogoficial38@gmail.com>
2024-02-25 00:09:52 -0300

    refactor: remove test pipeline

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const app = express();
 const ENV = process.env.NODE_ENV;
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('ok'))
-
 app.post('/api/v1/streams', streamController.uploadVideo)
 
 app.listen(PORT, () => {
